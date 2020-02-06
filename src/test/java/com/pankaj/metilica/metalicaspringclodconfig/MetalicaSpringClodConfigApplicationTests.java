deb5179b9148372f2bfb98e5809dbbda589b9464
pankajrana737 <pankajrana737@gmail.com>
2020-02-06 16:52:21 +0530

    new file:   .gitignore 	new file:   .mvn/wrapper/MavenWrapperDownloader.java 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml new file:   src/main/java/com/pankaj/metilica/metalicaspringclodconfig/MetalicaSpringClodConfigApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/pankaj/metilica/metalicaspringclodconfig/MetalicaSpringClodConfigApplicationTests.java

---
+++ src/test/java/com/pankaj/metilica/metalicaspringclodconfig/MetalicaSpringClodConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankaj.metilica.metalicaspringclodconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalicaSpringClodConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
